35c5e57b7641b4707a0720c527ba1aec774ee6e1
ErickAbanto <aeriatab@gmail.com>
2025-12-14 17:54:49 -0500

    ELIMINAR: botón de WhatsApp en el componente HomePage

--- pizzeria-react-frontend/src/components/pages/HomePage.jsx
+++ pizzeria-react-frontend/src/components/pages/HomePage.jsx
@@ -66,17 +66,6 @@ function HomePage() {
         </div>
       </div>
 
-      {/* BOTÓN WHATSAPP (YA USA showWhatsApp) */}
-      {showWhatsApp && (
-        <a
-          href="https://wa.me/51910151588"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:scale-110 transition"
-        >
-          <FaWhatsapp size={28} />
-        </a>
-      )}
 
       {/* FOOTER */}
       <footer className="bg-black text-white mt-12 py-8">
